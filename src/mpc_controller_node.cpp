9f4e43bcd12b58326152d40d6afa8e910385ecea
PapaVeneti <michael.d.papadakis@gmail.com>
2024-06-04 19:20:22 +0300

    play trajectory from mpc in drake

--- src/mpc_controller_node.cpp
+++ src/mpc_controller_node.cpp
@@ -9,6 +9,23 @@
 #define corresponding_kinematic_configuration(leg_id) ( (i==0 || i==3)? 1: 2 ) 
 #define traj_stages 4
 
+#include <stdlib.h>
+// acados
+#include "acados/utils/math.h"
+#include "acados_c/ocp_nlp_interface.h"
+#include "acados_c/external_function_interface.h"
+#include "c_generated_code/acados_solver_fr_leg_pos.h" 
+
+// blasfeo
+#include "blasfeo/include/blasfeo_d_aux_ext_dep.h"
+
+//OCP defines:
+#define NX     FR_LEG_POS_NX 
+#define NU     FR_LEG_POS_NU 
+#define NBX0   FR_LEG_POS_NBX0 
+
+#define TH    0.3
+
 //This are read from a config in WBC class
 struct leg_controller_opt{
   std::array<int,5> joint_state_id{0,1,2,3,4};
@@ -40,6 +57,7 @@ class leg_controller {
       //Debugging:
       std::cout << "Current id is: " << id << std::endl;
       for (auto id:joint_state_id){std::cout << id << std::endl;}
+      std::cout <<"Kinematic config is:" << opts.kinematic_config << std::endl;
       std::cout << "====" << std::endl;
       for (auto id:setpoint_arr.ids){std::cout << int(id) << std::endl;}
 
@@ -57,6 +75,18 @@ class leg_controller {
         setpoint_arr.values = qd_vec;
         Publish();
       }
+  }
+
+  Eigen::Matrix<double,5,1> leg_state_estimation(const Eigen::Vector3d & qd){
+    return kinematics.state_estimation(qd);
+  }
+
+  Eigen::Matrix<double,10,1>  get_full_state() const {
+    return full_state; 
+  }
+
+  const Eigen::Matrix<double,10,1> *  get_full_state_ptr() const {
+    return &full_state; 
   }
 
   private:
@@ -154,13 +184,109 @@ class whole_body_controller {
 
       //3. trajectory timer
       trajectory_timer = nh.createTimer(5,&whole_body_controller::trajectory_publish,this);
-      trajectory_timer.setPeriod( ros::Duration(1) );
-      trajectory_timer.start();
-      ;
+      trajectory_timer.setPeriod( ros::Duration(TH/FR_LEG_POS_N) );
+      trajectory_timer.stop();
+      // trajectory_timer.start();
+      
+      xtraj.fill(0);
+      utraj.fill(0);
+      dt_traj.fill( TH/FR_LEG_POS_N );
+
+      //MPC initialization:
+      mpc_solver_init();
     }
-  
+
+  private:
+  //MPC specific:
+  void mpc_solver_init(){
+    acados_ocp_capsule = fr_leg_pos_acados_create_capsule();
+    double* new_time_steps = NULL;
+    int status = fr_leg_pos_acados_create_with_discretization(acados_ocp_capsule, N, new_time_steps);
+
+    if (status)
+    {
+        printf("fr_leg_pos_acados_create() returned status %d. Exiting.\n", status);
+        exit(1);
+    }
+
+    nlp_config = fr_leg_pos_acados_get_nlp_config(acados_ocp_capsule);
+    nlp_dims = fr_leg_pos_acados_get_nlp_dims(acados_ocp_capsule);
+    nlp_in = fr_leg_pos_acados_get_nlp_in(acados_ocp_capsule);
+    nlp_out = fr_leg_pos_acados_get_nlp_out(acados_ocp_capsule);
+    nlp_solver = fr_leg_pos_acados_get_nlp_solver(acados_ocp_capsule);
+    nlp_opts = fr_leg_pos_acados_get_nlp_opts(acados_ocp_capsule);
+
+    //constraint ids:
+    for (int id=0; id< NBX0 ; id++){ idxbx[id]=id; }
+  }
+
+  void mpc_update_constraints(double *x0, double * xf){
+    ocp_nlp_constraints_model_set(nlp_config, nlp_dims, nlp_in, 0, "idxbx", idxbx.data());
+    ocp_nlp_constraints_model_set(nlp_config, nlp_dims, nlp_in, 0, "lbx", x0);
+    ocp_nlp_constraints_model_set(nlp_config, nlp_dims, nlp_in, 0, "ubx", x0);
+
+    ocp_nlp_constraints_model_set(nlp_config, nlp_dims, nlp_in, N, "idxbx", idxbx.data());
+    ocp_nlp_constraints_model_set(nlp_config, nlp_dims, nlp_in, N, "lbx", xf);
+    ocp_nlp_constraints_model_set(nlp_config, nlp_dims, nlp_in, N, "ubx", xf);
+  }
+
+  void initialize_mpc(Eigen::Matrix<double,10,1> & x0, Eigen::Matrix<double,10,1> & xf ){
+    Eigen::Vector3d u0;
+    u0.setZero();
+
+    for (int i = 0; i < N; i++)
+    {
+        ocp_nlp_out_set(nlp_config, nlp_dims, nlp_out, i, "x", x0.data());
+        ocp_nlp_out_set(nlp_config, nlp_dims, nlp_out, i, "u", u0.data());
+    }
+    ocp_nlp_out_set(nlp_config, nlp_dims, nlp_out, N, "x", x0.data());
+  }
+
+  public:
+  void mpc_update_ocp(const Eigen::Vector3d & qdesired){
+    auto qd = leg_controllers[0]->leg_state_estimation(qdesired);
+
+    //TODO: fix kinematics to use setpoint based on command:
+    qd <<  0,1.8000,-1.1703,-0.7000,0.4087;
+
+    Eigen::Matrix<double,10,1> xd ;
+    xd.setZero();
+    xd.head(5) = qd;
+
+    ROS_INFO_STREAM("setpoint is:" <<qd); //devugging:
+
+    Eigen::Matrix<double,10,1> x0 ;
+    x0 = leg_controllers[0]->get_full_state()*M_PI/180;
+
+    mpc_update_constraints(x0.data(),xd.data());
+    initialize_mpc(x0,xd);
+
+    //update config
+    ocp_nlp_solver_opts_set(nlp_config, nlp_opts, "rti_phase", &mpc_counter);
+
+    //Solve: 
+    int status = fr_leg_pos_acados_solve(acados_ocp_capsule);
+
+    double elapsed_time;
+    //Get data:
+    ocp_nlp_get(nlp_config, nlp_solver, "time_tot", &elapsed_time);
+    elapsed_time = MIN(elapsed_time, 1e12);
+
+    //get solution
+    for (int ii = 0; ii <= nlp_dims->N; ii++)
+        ocp_nlp_out_get(nlp_config, nlp_dims, nlp_out, ii, "x", &xtraj[ii*NX]);
+    for (int ii = 0; ii < nlp_dims->N; ii++)
+        ocp_nlp_out_get(nlp_config, nlp_dims, nlp_out, ii, "u", &utraj[ii*NU]);
+
+    ROS_INFO("[mpc_controller]: MPC exited with status:  %d. Elapsed time: %.1f [ms]",status,1e3*elapsed_time);
+    trajectory_indexer = 0;
+    trajectory_timer.start();
+
+  }
+
   private:
 
+  //Controller support functions:
   void allocation(const Eigen::Vector3f & qd_fr){
     //initialize
     Eigen::Vector3f qd_rr;
@@ -200,13 +326,22 @@ class whole_body_controller {
   }
 
   void trajectory_publish(const ros::TimerEvent&){
-    int & i = trajectory_indexer;
-    Eigen::Vector3f pos(qMH_t[i],qHI_t[i],qHO_t[i]);
-    allocation(pos);
-    trajectory_timer.setPeriod( ros::Duration( traj_dt[i]) );
-    
+    int & i = trajectory_indexer ;
     i++;
-    if (i == traj_stages){i=0;}
+
+    // Eigen::Vector3f pos(qMH_t[i],qHI_t[i],qHO_t[i]);
+    double & qMH = xtraj[i*NX    ];
+    double & qHI = xtraj[i*NX +1 ];
+    double & qHO = xtraj[i*NX +3 ];
+
+    Eigen::Vector3f pos(qMH,qHI,qHO);
+    allocation(pos*180/M_PI);
+    trajectory_timer.setPeriod( ros::Duration( dt_traj[i-1]) );
+    
+    if (i == FR_LEG_POS_N+1){
+      i=0;
+      trajectory_timer.stop();
+    }
   }
 
   bool cancel_roll = true;
@@ -217,10 +352,26 @@ class whole_body_controller {
 
   //Saved trajectory:
   int trajectory_indexer = 0;
-  std::vector<double> qMH_t{0.0, 0.0, 0.0, 0.0};
-  std::vector<double> qHI_t{110.0, -60.0, 0.0, 100.0};
-  std::vector<double> qHO_t{110.0, 230.0, 0.0, -60.0};
-  std::vector<double> traj_dt {1, 1, 0.5, 1}; //dt
+
+  std::array<double,NX*(FR_LEG_POS_N+1)> xtraj;
+  std::array<double,NU* FR_LEG_POS_N   > utraj;
+  std::array<double,NX* FR_LEG_POS_N   > dt_traj;
+
+  //MPC specific:
+  fr_leg_pos_solver_capsule *acados_ocp_capsule;
+  ocp_nlp_config *nlp_config ;
+  ocp_nlp_dims *nlp_dims ;
+  ocp_nlp_in *nlp_in ;
+  ocp_nlp_out *nlp_out ;
+  ocp_nlp_solver *nlp_solver ;
+  void *nlp_opts ;
+
+  std::array<int,NBX0> idxbx;
+  int N = FR_LEG_POS_N; //Number of shooting intervals
+
+  //stats:
+  int mpc_counter = 0;
+
 };
 
 int main(int argc, char **argv) {
@@ -235,6 +386,11 @@ ros::Rate loop_rate(loop_freq); //HZ
 #pragma endregion
 
 whole_body_controller wbc;
+
+Eigen::Vector3d qd(0,1.8000,-0.7000);
+auto timer  = nh.createTimer(ros::Duration(5),
+[&wbc, qd](const ros::TimerEvent&) { wbc.mpc_update_ocp(qd); },true,true);
+
 
 while(ros::ok()){
   ros::spinOnce(); // Process any callbacks
--- src/test_node.cpp
+++ src/test_node.cpp
@@ -1,9 +1,8 @@
 #include <ros/ros.h>
 //link with generated library:
-#include "c_generated_code/acados_solver_fr_leg_pos.h" 
 //with ACADOS_INCLUDE_DIRS
 // #include "acados/utils/types.h"
-
+#include "eigen3/Eigen/Dense"
 
 // standard
 #include <stdio.h>
@@ -13,40 +12,41 @@
 #include "acados/utils/math.h"
 #include "acados_c/ocp_nlp_interface.h"
 #include "acados_c/external_function_interface.h"
+#include "c_generated_code/acados_solver_fr_leg_pos.h" 
 // #include "acados_solver_fr_leg_pos.h"
 
 // blasfeo
 #include "blasfeo/include/blasfeo_d_aux_ext_dep.h"
 
-#define NX     FR_LEG_POS_NX
-#define NZ     FR_LEG_POS_NZ
-#define NU     FR_LEG_POS_NU
-#define NP     FR_LEG_POS_NP
-#define NBX    FR_LEG_POS_NBX
-#define NBX0   FR_LEG_POS_NBX0
-#define NBU    FR_LEG_POS_NBU
-#define NSBX   FR_LEG_POS_NSBX
-#define NSBU   FR_LEG_POS_NSBU
-#define NSH    FR_LEG_POS_NSH
-#define NSG    FR_LEG_POS_NSG
-#define NSPHI  FR_LEG_POS_NSPHI
-#define NSHN   FR_LEG_POS_NSHN
-#define NSGN   FR_LEG_POS_NSGN
-#define NSPHIN FR_LEG_POS_NSPHIN
-#define NSBXN  FR_LEG_POS_NSBXN
-#define NS     FR_LEG_POS_NS
-#define NSN    FR_LEG_POS_NSN
-#define NG     FR_LEG_POS_NG
-#define NBXN   FR_LEG_POS_NBXN
-#define NGN    FR_LEG_POS_NGN
-#define NY0    FR_LEG_POS_NY0
-#define NY     FR_LEG_POS_NY
-#define NYN    FR_LEG_POS_NYN
-#define NH     FR_LEG_POS_NH
-#define NPHI   FR_LEG_POS_NPHI
-#define NHN    FR_LEG_POS_NHN
-#define NPHIN  FR_LEG_POS_NPHIN
-#define NR     FR_LEG_POS_NR
+#define NX     FR_LEG_POS_NX //used
+// #define NZ     FR_LEG_POS_NZ
+#define NU     FR_LEG_POS_NU //used
+// #define NP     FR_LEG_POS_NP
+// #define NBX    FR_LEG_POS_NBX
+#define NBX0   FR_LEG_POS_NBX0 //used
+// #define NBU    FR_LEG_POS_NBU
+// #define NSBX   FR_LEG_POS_NSBX
+// #define NSBU   FR_LEG_POS_NSBU
+// #define NSH    FR_LEG_POS_NSH
+// #define NSG    FR_LEG_POS_NSG
+// #define NSPHI  FR_LEG_POS_NSPHI
+// #define NSHN   FR_LEG_POS_NSHN
+// #define NSGN   FR_LEG_POS_NSGN
+// #define NSPHIN FR_LEG_POS_NSPHIN
+// #define NSBXN  FR_LEG_POS_NSBXN
+// #define NS     FR_LEG_POS_NS
+// #define NSN    FR_LEG_POS_NSN
+// #define NG     FR_LEG_POS_NG
+// #define NBXN   FR_LEG_POS_NBXN
+// #define NGN    FR_LEG_POS_NGN
+// #define NY0    FR_LEG_POS_NY0
+// #define NY     FR_LEG_POS_NY
+// #define NYN    FR_LEG_POS_NYN
+// #define NH     FR_LEG_POS_NH
+// #define NPHI   FR_LEG_POS_NPHI
+// #define NHN    FR_LEG_POS_NHN
+// #define NPHIN  FR_LEG_POS_NPHIN
+// #define NR     FR_LEG_POS_NR
 
 int main(int argc, char **argv) {
 
@@ -84,120 +84,69 @@ fr_leg_pos_solver_capsule *acados_ocp_capsule = fr_leg_pos_acados_create_capsule
     ocp_nlp_solver *nlp_solver = fr_leg_pos_acados_get_nlp_solver(acados_ocp_capsule);
     void *nlp_opts = fr_leg_pos_acados_get_nlp_opts(acados_ocp_capsule);
 
-    // initial condition
-    int idxbx0[NBX0];
-    idxbx0[0] = 0;
-    idxbx0[1] = 1;
-    idxbx0[2] = 2;
-    idxbx0[3] = 3;
-    idxbx0[4] = 4;
-    idxbx0[5] = 5;
-    idxbx0[6] = 6;
-    idxbx0[7] = 7;
-    idxbx0[8] = 8;
-    idxbx0[9] = 9;
 
-    double lbx0[NBX0];
-    double ubx0[NBX0];
-    lbx0[0] = 0;
-    ubx0[0] = 0;
-    lbx0[1] = 0;
-    ubx0[1] = 0;
-    lbx0[2] = 0;
-    ubx0[2] = 0;
-    lbx0[3] = 0;
-    ubx0[3] = 0;
-    lbx0[4] = 0;
-    ubx0[4] = 0;
-    lbx0[5] = 0;
-    ubx0[5] = 0;
-    lbx0[6] = 0;
-    ubx0[6] = 0;
-    lbx0[7] = 0;
-    ubx0[7] = 0;
-    lbx0[8] = 0;
-    ubx0[8] = 0;
-    lbx0[9] = 0;
-    ubx0[9] = 0;
+    //Set initial conditions:
+    #pragma region 
+    std::array<int,NBX0> idxbx;
+    for (int id=0; id< NBX0 ; id++){ idxbx[id]=id; }
+    Eigen::Matrix<double,NBX0,1> x0 ;
+    x0.setZero();   
+    Eigen::Vector3d u0;
+    u0.setZero();
 
-    double lbxf[NBX0];
-    double ubxf[NBX0];
-    lbxf[0] = 0;
-    ubxf[0] = 0;
-    lbxf[1] = 1.8000;
-    ubxf[1] = 1.8000;
-    lbxf[2] = -1.1703;
-    ubxf[2] = -1.1703;
-    lbxf[3] = -0.7000;
-    ubxf[3] = -0.7000;
-    lbxf[4] = 0.4087;
-    ubxf[4] = 0.4087;
-    lbxf[5] = 0;
-    ubxf[5] = 0;
-    lbxf[6] = 0;
-    ubxf[6] = 0;
-    lbxf[7] = 0;
-    ubxf[7] = 0;
-    lbxf[8] = 0;
-    ubxf[8] = 0;
-    lbxf[9] = 0;
-    ubxf[9] = 0;
+    //Set target:
+    Eigen::Matrix<double,NBX0,1> xf ;
+    Eigen::Matrix<double,5,1>  qref; 
+    qref <<  0,1.8000,-1.1703,-0.7000,0.4087;    
+    xf.setZero();
+    xf.head(5) = qref;
 
-    ocp_nlp_constraints_model_set(nlp_config, nlp_dims, nlp_in, 0, "idxbx", idxbx0);
-    ocp_nlp_constraints_model_set(nlp_config, nlp_dims, nlp_in, 0, "lbx", lbx0);
-    ocp_nlp_constraints_model_set(nlp_config, nlp_dims, nlp_in, 0, "ubx", ubx0);
+    //Set constraints 
+    ocp_nlp_constraints_model_set(nlp_config, nlp_dims, nlp_in, 0, "idxbx", idxbx.data());
+    ocp_nlp_constraints_model_set(nlp_config, nlp_dims, nlp_in, 0, "lbx", x0.data());
+    ocp_nlp_constraints_model_set(nlp_config, nlp_dims, nlp_in, 0, "ubx", x0.data());
 
-    ocp_nlp_constraints_model_set(nlp_config, nlp_dims, nlp_in, N, "idxbx", idxbx0);
-    ocp_nlp_constraints_model_set(nlp_config, nlp_dims, nlp_in, N, "lbx", lbxf);
-    ocp_nlp_constraints_model_set(nlp_config, nlp_dims, nlp_in, N, "ubx", ubxf);
-
-    // initialization for state values
-    double x_init[NX];
-    x_init[0] = 0.0;
-    x_init[1] = 0.0;
-    x_init[2] = 0.0;
-    x_init[3] = 0.0;
-    x_init[4] = 0.0;
-    x_init[5] = 0.0;
-    x_init[6] = 0.0;
-    x_init[7] = 0.0;
-    x_init[8] = 0.0;
-    x_init[9] = 0.0;
-
-    // initial value for control input
-    double u0[NU];
-    u0[0] = 0.0;
-    u0[1] = 0.0;
-    u0[2] = 0.0;
+    ocp_nlp_constraints_model_set(nlp_config, nlp_dims, nlp_in, N, "idxbx", idxbx.data());
+    ocp_nlp_constraints_model_set(nlp_config, nlp_dims, nlp_in, N, "lbx", xf.data());
+    ocp_nlp_constraints_model_set(nlp_config, nlp_dims, nlp_in, N, "ubx", xf.data());
+    #pragma endregion
 
     // prepare evaluation
-    int NTIMINGS = 1;
+    // int NTIMINGS = 1;
     double min_time = 1e12;
     double kkt_norm_inf;
     double elapsed_time;
     int sqp_iter;
 
-    double xtraj[NX * (N+1)];
-    double utraj[NU * N];
+    std::array<double,NX*(FR_LEG_POS_N+1)> xtraj;
+    std::array<double,NU* FR_LEG_POS_N   > utraj;
 
 
     // solve ocp in loop
     int rti_phase = 0;
 
-    for (int ii = 0; ii < NTIMINGS; ii++)
+    // for (int ii = 0; ii < NTIMINGS; ii++)
+    // {
+
+    // initialize solution
+    for (int i = 0; i < N; i++)
     {
-        // initialize solution
-        for (int i = 0; i < N; i++)
-        {
-            ocp_nlp_out_set(nlp_config, nlp_dims, nlp_out, i, "x", x_init);
-            ocp_nlp_out_set(nlp_config, nlp_dims, nlp_out, i, "u", u0);
-        }
-        ocp_nlp_out_set(nlp_config, nlp_dims, nlp_out, N, "x", x_init);
-        ocp_nlp_solver_opts_set(nlp_config, nlp_opts, "rti_phase", &rti_phase);
-        status = fr_leg_pos_acados_solve(acados_ocp_capsule);
-        ocp_nlp_get(nlp_config, nlp_solver, "time_tot", &elapsed_time);
-        min_time = MIN(elapsed_time, min_time);
+        ocp_nlp_out_set(nlp_config, nlp_dims, nlp_out, i, "x", x0.data());
+        ocp_nlp_out_set(nlp_config, nlp_dims, nlp_out, i, "u", u0.data());
     }
+    ocp_nlp_out_set(nlp_config, nlp_dims, nlp_out, N, "x", x0.data());
+
+    //update config
+    ocp_nlp_solver_opts_set(nlp_config, nlp_opts, "rti_phase", &rti_phase);
+
+    //Solve: 
+    status = fr_leg_pos_acados_solve(acados_ocp_capsule);
+
+    //Get data:
+    ocp_nlp_get(nlp_config, nlp_solver, "time_tot", &elapsed_time);
+    min_time = MIN(elapsed_time, min_time);
+
+    // }
 
     /* print solution and statistics */
     for (int ii = 0; ii <= nlp_dims->N; ii++)
@@ -205,13 +154,17 @@ fr_leg_pos_solver_capsule *acados_ocp_capsule = fr_leg_pos_acados_create_capsule
     for (int ii = 0; ii < nlp_dims->N; ii++)
         ocp_nlp_out_get(nlp_config, nlp_dims, nlp_out, ii, "u", &utraj[ii*NU]);
 
+    //Actual printing
+    #pragma region
+
+
     printf("\n--- xtraj ---\n");
-    d_print_exp_tran_mat( NX, N+1, xtraj, NX);
+    d_print_exp_tran_mat( NX, N+1, xtraj.data(), NX);
     printf("\n--- utraj ---\n");
-    d_print_exp_tran_mat( NU, N, utraj, NU );
+    d_print_exp_tran_mat( NU, N, utraj.data(), NU );
     // ocp_nlp_out_print(nlp_solver->dims, nlp_out);
 
-    printf("\nsolved ocp %d times, solution printed above\n\n", NTIMINGS);
+    // printf("\nsolved ocp %d times, solution printed above\n\n", NTIMINGS);
 
     if (status == ACADOS_SUCCESS)
     {
@@ -229,10 +182,13 @@ fr_leg_pos_solver_capsule *acados_ocp_capsule = fr_leg_pos_acados_create_capsule
     fr_leg_pos_acados_print_stats(acados_ocp_capsule);
 
     printf("\nSolver info:\n");
-    printf(" SQP iterations %2d\n minimum time for %d solve %f [ms]\n KKT %e\n",
-           sqp_iter, NTIMINGS, min_time*1000, kkt_norm_inf);
+    // printf(" SQP iterations %2d\n minimum time for %d solve %f [ms]\n KKT %e\n",
+        //    sqp_iter, NTIMINGS, min_time*1000, kkt_norm_inf);
 
-    // free solver
+    #pragma endregion    
+
+
+    // free solver -> After while
     status = fr_leg_pos_acados_free(acados_ocp_capsule);
     if (status) {
         printf("fr_leg_pos_acados_free() returned status %d. \n", status);
