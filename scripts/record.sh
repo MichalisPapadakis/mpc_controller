9913ecdaf8cc8d4ab0c95903a4089d5828cec87c
PapaVeneti <michael.d.papadakis@gmail.com>
2024-06-08 19:45:37 +0300

    added helper scripts to process results from controller

---
+++ scripts/record.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  echo "No rosbag name provided. Using default."
+  rosbag_name="olympus_mpc_rosbag"
+elif [ $# -eq 1 ]; then 
+    rosbag_name=$1 
+fi
+mkdir $rosbag_name
+cd $rosbag_name
+
+rosbag record /leg2_node/motor_statuses /qualisys/olympus/odom /leg_mpc_status /controller_current_phase -O $rosbag_name
+
+rostopic echo -b $rosbag_name.bag /leg2_node/motor_statuses -p > $rosbag_name\_motor_statuses.txt
+rostopic echo -b $rosbag_name.bag /qualisys/olympus/odom    -p > $rosbag_name\_odom.txt
+rostopic echo -b $rosbag_name.bag /leg_mpc_status           -p > $rosbag_name\_mpc_status.txt
+rostopic echo -b $rosbag_name.bag /controller_current_phase -p > $rosbag_name\_controller_phase.txt
+
+echo "Exported the topics in txts"
